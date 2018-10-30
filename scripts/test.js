08b92b653b89496de2e6b6dc45c66757fa6d6e7a
Bruce Mcpherson <bruce@mcpher.com>
2018-10-30 16:38:57 +0000

    clean

--- scripts/test.js
+++
@@ -1,4 +0,0 @@
-function addANamedRange() {
-  var doc = DocumentApp.openById("1VidXqzAzuRHUMHDu9K_hSgA46UmOWPwFRcEzDEs1J8A");
-  doc.addNamedRange("Watcher", doc.newRange().addElement(doc.getBody().getParagraphs()[2]).build());
-}
